6c3d6cb88324f6c6aa2f5cb67ce81dd51b3a9fda
basker19moon <basker19moon@gmail.com>
2022-12-04 12:55:11 +0530

    Again a New line added

--- myfristpythonprogram.py
+++ myfristpythonprogram.py
@@ -2,3 +2,5 @@ print("Hello!, World") # As you know every body writes same Hello! world as thei
 print('What the heaven is happening here!')
 print("Nothing was heppening over here my bro!") #fun line added...! 
 print("I got your point dude..!")
+print("That's great to hear from you bro!")
+##This is all for GIT for practice..!
